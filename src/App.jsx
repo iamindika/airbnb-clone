b62ec086b84e5b868cea37913c2e5897ddfafd48
Indika Tantrege <indika.kolamba@ca3cmam0f177wmd.ht.home>
2022-10-14 03:12:01 -0400

    render cardlist with a data prop from mock api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import CardList from './components/CardList';
+import data from './api/data';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <CardList data={data} />
     </div>
   )
 }
